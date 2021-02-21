a87c826fc9008245cf27481938f1366f0ce97ab2
Cemil Okay <cemilokay@gmail.com>
2021-02-21 15:42:29 +0000

    Fix empty input addition

--- scripts.js
+++ scripts.js
@@ -9,16 +9,20 @@
 function addItem(){
   // Access input box as a node
   let inputBox = document.getElementById('todo_input');
-  // Create li element for HTML
-  const listItem = document.createElement('li');
-  // Add a class to the li element
-  listItem.className = 'todo-list-item';
-  // Assign input box value to li element's innerHTML
-  listItem.innerHTML = inputBox.value;
-  // Insert newly created li element above to ul element.
-  document.getElementById('todo_list').appendChild(listItem);
-  // Empty input box value
-  inputBox.value = '';
+  if (inputBox.value) {
+    // Create li element for HTML
+    const listItem = document.createElement('li');
+    // Add a class to the li element
+    listItem.className = 'todo-list-item';
+    // Assign input box value to li element's innerHTML
+    listItem.innerHTML = inputBox.value;
+    // Insert newly created li element above to ul element.
+    document.getElementById('todo_list').appendChild(listItem);
+    // Empty input box value
+    inputBox.value = '';
+  } else {
+    alert('Input box cannot be empty')
+  }
 }
 
 // When User click on the item in the list, the item will be removed/deleted.
