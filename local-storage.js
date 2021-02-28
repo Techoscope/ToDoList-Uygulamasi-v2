20523a1c5cc719c4a893dc0ad1c19fe703b7be06
Cemil Okay <cemilokay@gmail.com>
2021-02-28 13:01:11 +0000

    Remove item from localStorage

--- local-storage.js
+++ local-storage.js
@@ -1,3 +1,6 @@
+// CRUD - Create, Read, Update, Delete,
+// BREAD - Browse, Read, Edit, Add, Delete
+
 let todoItems = [];
 
 function addItem(){
@@ -8,13 +11,26 @@ function addItem(){
   getItems();
 }
 
+// List all items
 function getItems() {
   const todoList =  document.getElementById('todo_list');
   todoItems = localStorage.getItem('todoItems').split(',');
   todoList.innerHTML = '';
   todoItems.forEach( item => {
-    todoList.innerHTML += '<li>' + item + '</li>';
+    todoList.innerHTML += '<li onclick="removeItem(this)">' + item + '</li>';
   })
+}
+
+// Tiklamis oldugum list item'in texti haric diger itemlardan filtrele
+// Filtrelenmis item'lari bir degiskene assign et
+// Bu yeni array'i local storage'a gonder
+
+function removeItem(element) {
+  const filteredArray = todoItems.filter(item => {
+    return item !== element.innerHTML;
+  })
+  localStorage.setItem('todoItems', filteredArray);
+  getItems();
 }
 
 getItems();
